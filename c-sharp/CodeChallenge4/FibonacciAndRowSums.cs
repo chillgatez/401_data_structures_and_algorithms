647a82cf41fbe4f6f138c5f8a3d9e5e6e64bec4b
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-24 18:19:53 -0500

    conflicts should be resolved

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -4,31 +4,41 @@ namespace CodeChallenges
   {
     public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
     {
-      int result = 0;
-
-      return result;
-    }
-
-    public static int[] RowSums(int[][] matrix)
-    {
-      int rows = matrix.Length;
-      int[] rowSums = new int[rows];
-
-      if (rows <= 0)
+      if (nthNumberInSequence <= 0)
       {
-        return new int[0];
+        throw new Exception("Input must be a posiive integer.");
+      }
+      if (nthNumberInSequence == 1)
+      {
+        return 0;
+      }
+      if (nthNumberInSequence == 2)
+      {
+        return 1;
       }
 
-      for (int i = 0; i < rows; i++)
-      {
-        for (int x = 0; x < matrix[i].Length; x++)
+
+      return GetNthNumberInFibonacciSequence(nthNumberInSequence - 1) + GetNthNumberInFibonacciSequence(nthNumberInSequence - 2);
+    }
+
+        public static int[] RowSums(int[][] matrix)
         {
-          rowSums[i] += matrix[i][x];
+            int rows = matrix.Length;
+            int[] rowSums = new int[rows];
+
+            if (rows <= 0)
+            {
+                return new int[0];
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int x = 0; x < matrix[i].Length; x++)
+                {
+                    rowSums[i] += matrix[i][x];
+                }
+            }
+
+            return rowSums;
         }
-      }
-
-      return rowSums;
     }
-  }
-
-}
