06099d0d58c8a2a4ee334f5f65479fd7ffa1fea4
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-24 18:12:49 -0500

    sum

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,19 +1,34 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge4
+  public class CodeChallenge4
+  {
+    public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
     {
-        public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
-        {
-            int result = 0;
+      int result = 0;
 
-            return result;
-        }
-
-        public static int[] RowSums(int[][] matrix)
-        {
-            int[] rowSums = new int[matrix[0].Length];
-            return rowSums;
-        }
-
+      return result;
     }
+
+    public static int[] RowSums(int[][] matrix)
+    {
+      int rows = matrix.Length;
+      int[] rowSums = new int[rows];
+
+      if (rows <= 0)
+      {
+        return new int[0];
+      }
+
+      for (int i = 0; i < rows; i++)
+      {
+        for (int x = 0; x < matrix[i].Length; x++)
+        {
+          rowSums[i] += matrix[i][x];
+        }
+      }
+
+      return rowSums;
+    }
+  }
+
 }
