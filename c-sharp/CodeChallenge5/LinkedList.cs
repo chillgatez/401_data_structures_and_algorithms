bb8bf74ca0c19a41a10f7c63138e63851e38d38c
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-24 10:36:14 -0500

    completed code challenge 6

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,15 +1,61 @@
-namespace CodeChallenges 
+using System.Text;
+
+namespace CodeChallenges
 {
-    public class LinkedList
+  public class LinkedList
+  {
+    private Node head;
+
+    public Node Head
     {
-        // public Node Head {get; set;}
-
-
-
+      get { return head; }
+      set { head = value; }
     }
 
-    public class Node
+    public void Insert(int value)
     {
-
+      Node newNode = new Node(value);
+      newNode.Next = Head;
+      Head = newNode;
     }
+
+    public bool Includes(int value)
+    {
+      Node current = Head;
+      while (current != null)
+      {
+        if (current.Value == value)
+        {
+          return true;
+        }
+        current = current.Next;
+      }
+      return false;
+    }
+
+    public override string ToString()
+    {
+      Node current = Head;
+      string result = "";
+      while (current != null)
+      {
+        result += $"{current.Value} -> ";
+        current = current.Next;
+      }
+      result += "NULL";
+      return result;
+    }
+  }
+
+  public class Node
+  {
+    public int Value { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value)
+    {
+      Value = value;
+      Next = null;
+    }
+  }
 }
--- c-sharp/CodeChallenge6/LinkedListInsert.cs
+++ c-sharp/CodeChallenge6/LinkedListInsert.cs
@@ -1,22 +1,100 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class InsertableLinkedList: LinkedList
-    {
+  public class InsertableLinkedList : LinkedList
+  {
 
-        //adds a new node with the given value to the end of the list
-        public void Append(int value)
+    //adds a new node with the given value to the end of the list
+    public void Append(int value)
+    {
+      Node newNode = new Node(value);
+
+      if (Head == null)
+      {
+        // If the list is empty, set the new node as the head
+        Head = newNode;
+      }
+      else
+      {
+        // Otherwise, traverse the list to find the last node
+        Node current = Head;
+        while (current.Next != null)
         {
-            
+          current = current.Next;
         }
-        //adds a new node with the given new value immediately before the first node that has the value specified
-        public void InsertBefore(int value, int valueToInsert)
-        {
-            
-        }
-        //adds a new node with the given new value immediately after the first node that has the value specified
-        public void InsertAfter(int value, int valueToInsert)
-        {
-            
-        }
+
+        // Add the new node to the end
+        current.Next = newNode;
+      }
     }
+    //adds a new node with the given new value immediately before the first node that has the value specified
+    public void InsertBefore(int value, int valueToInsert)
+    {
+      Node newNode = new Node(valueToInsert);
+
+      if (Head == null)
+      {
+        // If the list is empty, set the new node as the head
+        Head = newNode;
+      }
+      else if (Head.Value == value)
+      {
+        // If the first node matches the value, insert the new node before the current head
+        newNode.Next = Head;
+        Head = newNode;
+      }
+      else
+      {
+        // Otherwise, traverse the list to find the node with the specified value
+        Node current = Head;
+        while (current.Next != null && current.Next.Value != value)
+        {
+          current = current.Next;
+        }
+
+        // If the specified value is found, insert the new node before it
+        if (current.Next != null)
+        {
+          newNode.Next = current.Next;
+          current.Next = newNode;
+        }
+        else
+        {
+          // If the specified value is not found, throw an exception
+          throw new ArgumentException("Value not found in the linked list.");
+        }
+      }
+    }
+    //adds a new node with the given new value immediately after the first node that has the value specified
+    public void InsertAfter(int value, int valueToInsert)
+    {
+      Node newNode = new Node(valueToInsert);
+
+      if (Head == null)
+      {
+        // If the list is empty, set the new node as the head
+        Head = newNode;
+      }
+      else
+      {
+        // Traverse the list to find the node with the specified value
+        Node current = Head;
+        while (current != null && current.Value != value)
+        {
+          current = current.Next;
+        }
+
+        // If the specified value is found, insert the new node after it
+        if (current != null)
+        {
+          newNode.Next = current.Next;
+          current.Next = newNode;
+        }
+        else
+        {
+          // If the specified value is not found, throw an exception
+          throw new ArgumentException("Value not found in the linked list.");
+        }
+      }
+    }
+  }
 }
