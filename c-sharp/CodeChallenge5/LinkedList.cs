c0893fea322e745ebc24972c6077dc8f9cbb47e1
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-24 01:29:53 -0500

    Submission

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,19 +1,61 @@
-namespace CodeChallenges 
+using System.Text;
+
+namespace CodeChallenges
 {
-    public class LinkedList
-    {
+  public class LinkedList
+  {
+    private Node header;
 
-        public void AddNewNode()
-        {
-            
-        }
+    public Node Header
+    {
+      get { return header; }
+      set { header = value; }
     }
 
-    public class Node
+    public void AddNewNode(int value)
     {
-        public Node()
-        {
-
-        }
+      Node newNode = new Node(value);
+      newNode.Next = Header;
+      Header = newNode;
     }
+
+    public bool Contains(int value)
+    {
+      Node current = Header;
+      while (current != null)
+      {
+        if (current.Value == value)
+        {
+          return true;
+        }
+        current = current.Next;
+      }
+      return false;
+    }
+
+    public override string ToString()
+    {
+      Node current = Header;
+      StringBuilder result = new StringBuilder();
+      while (current != null)
+      {
+        result.Append($"{{ {current.Value} }} -> ");
+        current = current.Next;
+      }
+      result.Append("NULL");
+      return result.ToString();
+    }
+  }
+
+  public class Node
+  {
+    public int Value { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value)
+    {
+      Value = value;
+      Next = null;
+    }
+  }
 }
